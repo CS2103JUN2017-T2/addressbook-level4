ac3ffa7e3da5a0f734be9e308145bc323577dcfb
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-07-05 00:41:27 +0800

    Remove all trailing whitespace and auto-format MainApp.java.

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -39,158 +39,159 @@ import seedu.address.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 0, 0, true);
+	public static final Version VERSION = new Version(1, 0, 0, true);
 
-    private static final Logger logger = LogsCenter.getLogger(MainApp.class);
+	private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
-    protected Ui ui;
-    protected Logic logic;
-    protected Storage storage;
-    protected Model model;
-    protected Config config;
-    protected UserPrefs userPrefs;
+	protected Ui ui;
+	protected Logic logic;
+	protected Storage storage;
+	protected Model model;
+	protected Config config;
+	protected UserPrefs userPrefs;
 
+	@Override
+	public void init() throws Exception {
+		logger.info("=============================[ Initializing EntryBook ]===========================");
+		super.init();
 
-    @Override
-    public void init() throws Exception {
-        logger.info("=============================[ Initializing EntryBook ]===========================");
-        super.init();
+		config = initConfig(getApplicationParameter("config"));
 
-        config = initConfig(getApplicationParameter("config"));
+		UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
+		userPrefs = initPrefs(userPrefsStorage);
+		EntryBookStorage entryBookStorage = new XmlEntryBookStorage(userPrefs.getEntryBookFilePath());
+		storage = new StorageManager(entryBookStorage, userPrefsStorage);
 
-        UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
-        userPrefs = initPrefs(userPrefsStorage);
-        EntryBookStorage entryBookStorage = new XmlEntryBookStorage(userPrefs.getEntryBookFilePath());
-        storage = new StorageManager(entryBookStorage, userPrefsStorage);
+		initLogging(config);
 
-        initLogging(config);
+		model = initModelManager(storage, userPrefs);
 
-        model = initModelManager(storage, userPrefs);
+		logic = new LogicManager(model);
 
-        logic = new LogicManager(model);
+		ui = new UiManager(logic, config, userPrefs);
 
-        ui = new UiManager(logic, config, userPrefs);
+		initEventsCenter();
+	}
 
-        initEventsCenter();
-    }
+	private String getApplicationParameter(String parameterName) {
+		Map<String, String> applicationParameters = getParameters().getNamed();
+		return applicationParameters.get(parameterName);
+	}
 
-    private String getApplicationParameter(String parameterName) {
-        Map<String, String> applicationParameters = getParameters().getNamed();
-        return applicationParameters.get(parameterName);
-    }
+	private Model initModelManager(Storage storage, UserPrefs userPrefs) {
+		Optional<ReadOnlyEntryBook> entryBookOptional;
+		ReadOnlyEntryBook initialData;
+		try {
+			entryBookOptional = storage.readEntryBook();
+			if (!entryBookOptional.isPresent()) {
+				logger.info("Data file not found. Will be starting with an empty EntryBook");
+			}
+			initialData = new EntryBook();
+		} catch (DataConversionException e) {
+			logger.warning("Data file not in the correct format. Will be starting with an empty EntryBook");
+			initialData = new EntryBook();
+		} catch (IOException e) {
+			logger.warning("Problem while reading from the file. Will be starting with an empty EntryBook");
+			initialData = new EntryBook();
+		}
 
-    private Model initModelManager(Storage storage, UserPrefs userPrefs) {
-        Optional<ReadOnlyEntryBook> entryBookOptional;
-        ReadOnlyEntryBook initialData;
-        try {
-        	entryBookOptional = storage.readEntryBook();
-            if (!entryBookOptional.isPresent()) {
-                logger.info("Data file not found. Will be starting with an empty EntryBook");
-            }
-            initialData = new EntryBook();
-        } catch (DataConversionException e) {
-            logger.warning("Data file not in the correct format. Will be starting with an empty EntryBook");
-            initialData = new EntryBook();
-        } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty EntryBook");
-            initialData = new EntryBook();
-        }
+		return new ModelManager(initialData, userPrefs);
+	}
 
-        return new ModelManager(initialData, userPrefs);
-    }
+	private void initLogging(Config config) {
+		LogsCenter.init(config);
+	}
 
-    private void initLogging(Config config) {
-        LogsCenter.init(config);
-    }
+	protected Config initConfig(String configFilePath) {
+		Config initializedConfig;
+		String configFilePathUsed;
 
-    protected Config initConfig(String configFilePath) {
-        Config initializedConfig;
-        String configFilePathUsed;
+		configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
 
-        configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
+		if (configFilePath != null) {
+			logger.info("Custom Config file specified " + configFilePath);
+			configFilePathUsed = configFilePath;
+		}
 
-        if (configFilePath != null) {
-            logger.info("Custom Config file specified " + configFilePath);
-            configFilePathUsed = configFilePath;
-        }
+		logger.info("Using config file : " + configFilePathUsed);
 
-        logger.info("Using config file : " + configFilePathUsed);
+		try {
+			Optional<Config> configOptional = ConfigUtil.readConfig(configFilePathUsed);
+			initializedConfig = configOptional.orElse(new Config());
+		} catch (DataConversionException e) {
+			logger.warning("Config file at " + configFilePathUsed + " is not in the correct format. "
+					+ "Using default config properties");
+			initializedConfig = new Config();
+		}
 
-        try {
-            Optional<Config> configOptional = ConfigUtil.readConfig(configFilePathUsed);
-            initializedConfig = configOptional.orElse(new Config());
-        } catch (DataConversionException e) {
-            logger.warning("Config file at " + configFilePathUsed + " is not in the correct format. "
-                    + "Using default config properties");
-            initializedConfig = new Config();
-        }
+		// Update config file in case it was missing to begin with or there are
+		// new/unused fields
+		try {
+			ConfigUtil.saveConfig(initializedConfig, configFilePathUsed);
+		} catch (IOException e) {
+			logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+		}
+		return initializedConfig;
+	}
 
-        //Update config file in case it was missing to begin with or there are new/unused fields
-        try {
-            ConfigUtil.saveConfig(initializedConfig, configFilePathUsed);
-        } catch (IOException e) {
-            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
-        }
-        return initializedConfig;
-    }
+	protected UserPrefs initPrefs(UserPrefsStorage storage) {
+		String prefsFilePath = storage.getUserPrefsFilePath();
+		logger.info("Using prefs file : " + prefsFilePath);
 
-    protected UserPrefs initPrefs(UserPrefsStorage storage) {
-        String prefsFilePath = storage.getUserPrefsFilePath();
-        logger.info("Using prefs file : " + prefsFilePath);
+		UserPrefs initializedPrefs;
+		try {
+			Optional<UserPrefs> prefsOptional = storage.readUserPrefs();
+			initializedPrefs = prefsOptional.orElse(new UserPrefs());
+		} catch (DataConversionException e) {
+			logger.warning("UserPrefs file at " + prefsFilePath + " is not in the correct format. "
+					+ "Using default user prefs");
+			initializedPrefs = new UserPrefs();
+		} catch (IOException e) {
+			logger.warning("Problem while reading from the file. Will be starting with an empty EntryBook");
+			initializedPrefs = new UserPrefs();
+		}
 
-        UserPrefs initializedPrefs;
-        try {
-            Optional<UserPrefs> prefsOptional = storage.readUserPrefs();
-            initializedPrefs = prefsOptional.orElse(new UserPrefs());
-        } catch (DataConversionException e) {
-            logger.warning("UserPrefs file at " + prefsFilePath + " is not in the correct format. "
-                    + "Using default user prefs");
-            initializedPrefs = new UserPrefs();
-        } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty EntryBook");
-            initializedPrefs = new UserPrefs();
-        }
+		// Update prefs file in case it was missing to begin with or there are
+		// new/unused fields
+		try {
+			storage.saveUserPrefs(initializedPrefs);
+		} catch (IOException e) {
+			logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+		}
 
-        //Update prefs file in case it was missing to begin with or there are new/unused fields
-        try {
-            storage.saveUserPrefs(initializedPrefs);
-        } catch (IOException e) {
-            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
-        }
+		return initializedPrefs;
+	}
 
-        return initializedPrefs;
-    }
+	private void initEventsCenter() {
+		EventsCenter.getInstance().registerHandler(this);
+	}
 
-    private void initEventsCenter() {
-        EventsCenter.getInstance().registerHandler(this);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		logger.info("Starting EntryBook " + MainApp.VERSION);
+		ui.start(primaryStage);
+	}
 
-    @Override
-    public void start(Stage primaryStage) {
-        logger.info("Starting EntryBook " + MainApp.VERSION);
-        ui.start(primaryStage);
-    }
+	@Override
+	public void stop() {
+		logger.info("============================ [ Stopping EntryBook ] =============================");
+		ui.stop();
+		try {
+			storage.saveUserPrefs(userPrefs);
+		} catch (IOException e) {
+			logger.severe("Failed to save preferences " + StringUtil.getDetails(e));
+		}
+		Platform.exit();
+		System.exit(0);
+	}
 
-    @Override
-    public void stop() {
-        logger.info("============================ [ Stopping EntryBook ] =============================");
-        ui.stop();
-        try {
-            storage.saveUserPrefs(userPrefs);
-        } catch (IOException e) {
-            logger.severe("Failed to save preferences " + StringUtil.getDetails(e));
-        }
-        Platform.exit();
-        System.exit(0);
-    }
+	@Subscribe
+	public void handleExitAppRequestEvent(ExitAppRequestEvent event) {
+		logger.info(LogsCenter.getEventHandlingLogMessage(event));
+		this.stop();
+	}
 
-    @Subscribe
-    public void handleExitAppRequestEvent(ExitAppRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        this.stop();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
