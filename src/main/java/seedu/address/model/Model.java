7fce66b15caa126b6118793d5e8c41ec39b725a8
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-07-05 00:40:32 +0800

    Remove all trailing whitespace and auto-format all files in Model component.

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -10,33 +10,41 @@ import seedu.address.model.entry.exceptions.EntryNotFoundException;
  * The API of the Model component.
  */
 public interface Model {
-    /** Clears existing backing model and replaces with the provided new data. */
-    void resetData(ReadOnlyEntryBook newData);
+	/**
+	 * Clears existing backing model and replaces with the provided new data.
+	 */
+	void resetData(ReadOnlyEntryBook newData);
 
-    /** Returns the EntryBook */
-    ReadOnlyEntryBook getEntryBook();
+	/** Returns the EntryBook */
+	ReadOnlyEntryBook getEntryBook();
 
-    /** Deletes the given entry. */
-    void deleteEntry(ReadOnlyEntry target) throws EntryNotFoundException;
+	/** Deletes the given entry. */
+	void deleteEntry(ReadOnlyEntry target) throws EntryNotFoundException;
 
-    /** Adds the given entry */
-    void addEntry(ReadOnlyEntry entry);
+	/** Adds the given entry */
+	void addEntry(ReadOnlyEntry entry);
 
-    /**
-     * Replaces the given entry {@code target} with {@code editedEntry}.
-     *
-     * @throws EntryNotFoundException if {@code target} could not be found in the list.
-     */
-    void updateEntry(ReadOnlyEntry target, ReadOnlyEntry editedEntry)
-            throws EntryNotFoundException;
+	/**
+	 * Replaces the given entry {@code target} with {@code editedEntry}.
+	 *
+	 * @throws EntryNotFoundException
+	 *             if {@code target} could not be found in the list.
+	 */
+	void updateEntry(ReadOnlyEntry target, ReadOnlyEntry editedEntry) throws EntryNotFoundException;
 
-    /** Returns the filtered entry list as an {@code UnmodifiableObservableList<ReadOnlyEntry>} */
-    UnmodifiableObservableList<ReadOnlyEntry> getFilteredFloatingTaskList();
+	/**
+	 * Returns the filtered entry list as an
+	 * {@code UnmodifiableObservableList<ReadOnlyEntry>}
+	 */
+	UnmodifiableObservableList<ReadOnlyEntry> getFilteredFloatingTaskList();
 
-    /** Updates the filter of the filtered entry list to show all entries */
-    void updateFilteredListToShowAll();
+	/** Updates the filter of the filtered entry list to show all entries */
+	void updateFilteredListToShowAll();
 
-    /** Updates the filter of the filtered entry list to filter by the given keywords*/
-    void updateFilteredFloatingTaskList(Set<String> keywords);
+	/**
+	 * Updates the filter of the filtered entry list to filter by the given
+	 * keywords
+	 */
+	void updateFilteredFloatingTaskList(Set<String> keywords);
 
 }
