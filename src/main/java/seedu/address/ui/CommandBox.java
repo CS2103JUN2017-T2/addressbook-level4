a93a3048d0fff5053183cd326109854bfa6cb4fb
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-06 03:14:17 +0800

    Add use of arrow keys to switch through previous commands

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,10 +1,14 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
@@ -21,19 +25,46 @@ public class CommandBox extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private final Logic logic;
 
+    //@@author A0125586X
+    private ArrayList<String> commandTextHistory;
+    private int commandTextHistoryIdx;
+
     @FXML
     private TextField commandTextField;
 
     public CommandBox(Logic logic) {
         super(FXML);
         this.logic = logic;
+        setupHistory();
+    }
+
+    /**
+     * Sets up the components required to implement the command history function
+     * by using the up arrow key.
+     */
+    @FXML
+    private void setupHistory() {
+        commandTextHistory = new ArrayList<>();
+        commandTextHistory.add("");
+        commandTextHistoryIdx = 0;
+        getRoot().addEventHandler(KeyEvent.ANY, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.UP) {
+                    loadPreviousCommandIntoTextField();
+                } else if (event.getCode() == KeyCode.DOWN) {
+                    loadNextCommandIntoTextField();
+                }
+            }
+        });
     }
 
     @FXML
     private void handleCommandInputChanged() {
         try {
-            CommandResult commandResult = logic.execute(commandTextField.getText());
-
+            String commandText = commandTextField.getText();
+            saveCommandtoHistory(commandText);
+            CommandResult commandResult = logic.execute(commandText);
             // process result of the command
             setStyleToIndicateCommandSuccess();
             commandTextField.setText("");
@@ -48,6 +79,27 @@ public class CommandBox extends UiPart<Region> {
         }
     }
 
+    private void saveCommandtoHistory(String command) {
+        commandTextHistory.add(command);
+        commandTextHistoryIdx = commandTextHistory.size() - 1;
+    }
+
+    private void loadPreviousCommandIntoTextField() {
+        commandTextField.setText(commandTextHistory.get(commandTextHistoryIdx));
+        // Moves one step into the past
+        if (commandTextHistoryIdx > 0) {
+            --commandTextHistoryIdx;
+        }
+    }
+
+    private void loadNextCommandIntoTextField() {
+        // Moves one step towards the present
+        if (commandTextHistoryIdx < commandTextHistory.size() - 1) {
+            ++commandTextHistoryIdx;
+        }
+        commandTextField.setText(commandTextHistory.get(commandTextHistoryIdx));
+    }
+    //@@author
 
     /**
      * Sets the command box style to indicate a successful command.
