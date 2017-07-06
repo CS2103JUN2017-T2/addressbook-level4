44ca9accb444e479bb026dabd0a61ee9f8a64742
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-06 17:38:07 +0800

    Add remaining main window UI element handles

--- src/test/java/guitests/guihandles/MainWindowHandle.java
+++ src/test/java/guitests/guihandles/MainWindowHandle.java
@@ -10,29 +10,40 @@ import seedu.address.TestApp;
  */
 public class MainWindowHandle extends GuiHandle {
 
+    private final EventListPanelHandle eventListPanel;
+    private final DeadlineListPanelHandle deadlineListPanel;
     private final FloatingTaskListPanelHandle floatingTaskListPanel;
-
-    /*private final ResultDisplayHandle resultDisplay;
+    private final ResultDisplayHandle resultDisplay;
     private final CommandBoxHandle commandBox;
     private final StatusBarFooterHandle statusBarFooter;
-    private final MainMenuHandle mainMenu;*/
+    private final MainMenuHandle mainMenu;
 
     public MainWindowHandle(GuiRobot guiRobot, Stage primaryStage) {
         super(guiRobot, primaryStage, TestApp.APP_TITLE);
 
+        eventListPanel = new EventListPanelHandle(guiRobot, primaryStage);
+        deadlineListPanel = new DeadlineListPanelHandle(guiRobot, primaryStage);
         floatingTaskListPanel = new FloatingTaskListPanelHandle(guiRobot, primaryStage);
 
-        /*resultDisplay = new ResultDisplayHandle(guiRobot, primaryStage);
+        resultDisplay = new ResultDisplayHandle(guiRobot, primaryStage);
         commandBox = new CommandBoxHandle(guiRobot, primaryStage, TestApp.APP_TITLE);
         statusBarFooter = new StatusBarFooterHandle(guiRobot, primaryStage);
-        mainMenu = new MainMenuHandle(guiRobot, primaryStage);*/
+        mainMenu = new MainMenuHandle(guiRobot, primaryStage);
+    }
+
+    public EventListPanelHandle getEventListPanel() {
+        return eventListPanel;
+    }
+
+    public DeadlineListPanelHandle getDeadlineListPane() {
+        return deadlineListPanel;
     }
 
     public FloatingTaskListPanelHandle getFloatingTaskListPanel() {
         return floatingTaskListPanel;
     }
 
-    /*public ResultDisplayHandle getResultDisplay() {
+    public ResultDisplayHandle getResultDisplay() {
         return resultDisplay;
     }
 
@@ -46,6 +57,6 @@ public class MainWindowHandle extends GuiHandle {
 
     public MainMenuHandle getMainMenu() {
         return mainMenu;
-    }*/
+    }
 
 }
