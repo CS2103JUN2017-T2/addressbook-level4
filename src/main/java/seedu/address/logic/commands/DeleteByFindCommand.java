c42d92cf1ff4bc668b17a467a3ba3ab26c6d6077
kevinLamKB <e0004543@u.nus.edu>
2017-07-06 19:22:36 +0800

    Clean up DeleteCommand code, added 2 subclasses DeleteByFindCommand and DeleteByIndexCommand

---
+++ src/main/java/seedu/address/logic/commands/DeleteByFindCommand.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.commands;
+
+import java.util.Set;
+
+import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.entry.ReadOnlyEntry;
+import seedu.address.model.entry.exceptions.EntryNotFoundException;
+
+//@@author A0140633R
+/*
+ * Deletes an entry identified using the type of entry followed by displayed.
+ */
+public class DeleteByFindCommand extends DeleteCommand {
+    public static final String MESSAGE_DELETE_ENTRY_BY_FIND_FAIL = "More than one entry found! \n"
+            + " use delete + /float INDEX to delete";
+
+    private Set<String> keywords;
+
+    public DeleteByFindCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        UnmodifiableObservableList<ReadOnlyEntry> lastShownList = model.getFilteredFloatingTaskList();
+
+        model.updateFilteredFloatingTaskList(keywords);
+        if (model.getFilteredFloatingTaskList().size() == 1) {
+            entryToDelete = lastShownList.get(0);
+            try {
+                model.deleteEntry(entryToDelete);
+            } catch (EntryNotFoundException e) {
+                assert false : "The target entry cannot be missing";
+            }
+            return new CommandResult(String.format(MESSAGE_DELETE_ENTRY_SUCCESS, entryToDelete));
+        } else {
+            return new CommandResult(MESSAGE_DELETE_ENTRY_BY_FIND_FAIL);
+        }
+    }
+}
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ENTRYBOOK_FLOATINGTASK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FLOATINGTASK;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -11,6 +11,8 @@ import java.util.stream.Stream;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteByFindCommand;
+import seedu.address.logic.commands.DeleteByIndexCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -20,17 +22,24 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class DeleteCommandParser {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the
-     * DeleteCommand and returns an DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand and returns an
+     * DeleteCommand object for execution.
      *
      * @throws ParseException
-     * if the user input does not conform the expected format
+     *             if the user input does not conform the expected format
      */
-    //@@author A0140633R
+    // @@author A0140633R
     public Command parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ENTRYBOOK_FLOATINGTASK);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_FLOATINGTASK);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_ENTRYBOOK_FLOATINGTASK)) {
+        if (arePrefixesPresent(argMultimap, PREFIX_FLOATINGTASK)) {
+            try {
+                Index index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_FLOATINGTASK).get());
+                return new DeleteByIndexCommand(index);
+            } catch (IllegalValueException ive) {
+                throw new ParseException(ive.getMessage(), ive);
+            }
+        } else {
             String trimmedArgs = argMultimap.getPreamble().get();
             if (trimmedArgs.isEmpty()) {
                 throw new ParseException(
@@ -38,25 +47,14 @@ public class DeleteCommandParser {
             }
             final String[] keywords = trimmedArgs.split("\\s+");
             final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-            Index index = Index.fromZeroBased(0);
 
-            return new DeleteCommand(index, keywordSet);
-        }
-
-        try {
-            final Set<String> keywordSet = new HashSet<>();
-            Index index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_ENTRYBOOK_FLOATINGTASK).get());
-            System.out.println(argMultimap.getValue(PREFIX_ENTRYBOOK_FLOATINGTASK).get());
-            return new DeleteCommand(index, keywordSet);
-
-        } catch (IllegalValueException ive) {
-            throw new ParseException(ive.getMessage(), ive);
+            return new DeleteByFindCommand(keywordSet);
         }
     }
 
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional}
-     * values in the given {@code ArgumentMultimap}.
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
      */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
