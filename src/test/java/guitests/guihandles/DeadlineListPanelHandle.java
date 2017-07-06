dc29490bf82ee65e079d66b80a5f530ded797975
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-06 17:14:10 +0800

    Add list panel handles for the deadline and event lists

---
+++ src/test/java/guitests/guihandles/DeadlineListPanelHandle.java
@@ -0,0 +1,17 @@
+package guitests.guihandles;
+
+import guitests.GuiRobot;
+import javafx.stage.Stage;
+
+//@@author A0125586
+/**
+ * Provides a handle for the panel containing the deadline list.
+ */
+public class DeadlineListPanelHandle extends EntryListPanelHandle {
+
+    private static final String LIST_VIEW_ID = "#deadlineListView";
+
+    public DeadlineListPanelHandle(GuiRobot guiRobot, Stage primaryStage) {
+        super(guiRobot, primaryStage);
+    }
+}
