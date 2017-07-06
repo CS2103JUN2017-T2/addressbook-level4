1da6dc93ca1eeab1d221596fa8e3cf6e1cdd71a7
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-07-07 05:04:56 +0800

    Make entry name flexible, accepts non-alphanumeric characters except the first character.

--- src/main/java/seedu/address/model/entry/Name.java
+++ src/main/java/seedu/address/model/entry/Name.java
@@ -5,26 +5,30 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
- * Represents an Entry's name in the entry book.
- * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ * Represents an Entry's name in the entry book. Guarantees: immutable; is valid
+ * as declared in {@link #isValidName(String)}
  */
 public class Name {
 
-    public static final String MESSAGE_NAME_CONSTRAINTS =
-            "Entry names should only contain alphanumeric characters and spaces, and it should not be blank";
+    public static final String MESSAGE_NAME_CONSTRAINTS = "Entry names should only start "
+            + "with alphanumeric characters and it should not be blank, "
+            + "subsequent spaces and non-alphanumeric characters are allowed.";
 
     /*
-     * The first character of the entry must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
+     * The first character of the entry must not be a whitespace, otherwise " "
+     * (a blank string) becomes a valid input.
      */
-    public static final String NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    // public static final String NAME_VALIDATION_REGEX =
+    // "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum}\\p{S}\\p{P} ]*";
 
     public final String fullName;
 
     /**
      * Validates given name.
      *
-     * @throws IllegalValueException if given name string is invalid.
+     * @throws IllegalValueException
+     *             if given name string is invalid.
      */
     public Name(String name) throws IllegalValueException {
         requireNonNull(name);
@@ -42,7 +46,6 @@ public class Name {
         return test.matches(NAME_VALIDATION_REGEX);
     }
 
-
     @Override
     public String toString() {
         return fullName;
@@ -52,7 +55,8 @@ public class Name {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Name // instanceof handles nulls
-                && this.fullName.equals(((Name) other).fullName)); // state check
+                        && this.fullName.equals(((Name) other).fullName)); // state
+                                                                           // check
     }
 
     @Override
--- src/test/java/seedu/address/model/entry/NameTest.java
+++ src/test/java/seedu/address/model/entry/NameTest.java
@@ -17,17 +17,24 @@ public class NameTest {
         assertFalse(Name.isValidName("")); // empty string
         assertFalse(Name.isValidName(" ")); // spaces only
         assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric
-                                                 // characters
+        assertFalse(Name.isValidName("/test")); // non-alphanumeric as first
+                                                // character
 
         // valid name
-        assertTrue(Name.isValidName("peter jack")); // alphabets only
+        assertTrue(Name.isValidName("abc")); // joined alphabets only
+        assertTrue(Name.isValidName("peter jack")); // alphabets with space
+        assertTrue(Name.isValidName("peter jack ")); // trailing whitespace
         assertTrue(Name.isValidName("12345")); // numbers only
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric
                                                        // characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long
                                                                         // names
+        assertTrue(Name.isValidName("peter$")); // contains currency sign
+        assertTrue(Name.isValidName("peter?.,")); // punctuation marks
+        assertTrue(Name.isValidName("peter? . , / * &")); // punctuation marks
+                                                          // with space
+        // characters
     }
 
     // @@author A0126623L
