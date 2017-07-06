648eeb85734ca06c9f6856975ca047fe97319fe7
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-07-07 05:38:30 +0800

    More format adjust to make Travis happy.

--- src/test/java/seedu/address/model/entry/NameTest.java
+++ src/test/java/seedu/address/model/entry/NameTest.java
@@ -17,24 +17,19 @@ public class NameTest {
         assertFalse(Name.isValidName("")); // empty string
         assertFalse(Name.isValidName(" ")); // spaces only
         assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidName("/test")); // non-alphanumeric as first
-                                                // character
+        assertFalse(Name.isValidName("/test")); // non-alphanumeric as first character
 
         // valid name
         assertTrue(Name.isValidName("abc")); // joined alphabets only
         assertTrue(Name.isValidName("peter jack")); // alphabets with space
         assertTrue(Name.isValidName("peter jack ")); // trailing whitespace
         assertTrue(Name.isValidName("12345")); // numbers only
-        assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric
-                                                       // characters
+        assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
-        assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long
-                                                                        // names
+        assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
         assertTrue(Name.isValidName("peter$")); // contains currency sign
         assertTrue(Name.isValidName("peter?.,")); // punctuation marks
-        assertTrue(Name.isValidName("peter? . , / * &")); // punctuation marks
-                                                          // with space
-        // characters
+        assertTrue(Name.isValidName("peter? . , / * &")); // punctuation marks with space characters
     }
 
     // @@author A0126623L
@@ -48,10 +43,10 @@ public class NameTest {
             Name name3 = new Name("Jane");
 
             // equality
-            assertTrue(name1.equals(name2));
+            assertTrue(name1.equals(name2)); // Meaningfully equal
 
             // inequality
-            assertFalse(name1 == name2);
+            assertFalse(name1 == name3);
             assertFalse(name1.equals(name3));
 
         } catch (IllegalValueException e) {
