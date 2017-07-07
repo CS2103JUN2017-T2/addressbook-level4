fff98bde6e3b719e44c662724fdc569cfda173e0
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-07 18:34:28 +0800

    Add back last updated timestamp to UI status bar

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -1,28 +1,78 @@
 package seedu.address.ui;
 
+import java.time.Clock;
+import java.util.Date;
+import java.util.logging.Logger;
+
 import org.controlsfx.control.StatusBar;
+
+import com.google.common.eventbus.Subscribe;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.model.EntryBookChangedEvent;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
  */
 public class StatusBarFooter extends UiPart<Region> {
 
+    public static final String SYNC_STATUS_INITIAL = "Not updated yet in this session";
+    public static final String SYNC_STATUS_UPDATED = "Last Updated: %s";
+
+    private static Clock clock = Clock.systemDefaultZone();
+
+    private static final Logger logger = LogsCenter.getLogger(StatusBarFooter.class);
+
     private static final String FXML = "StatusBarFooter.fxml";
 
+    @FXML
+    private StatusBar syncStatus;
     @FXML
     private StatusBar saveLocationStatus;
 
 
     public StatusBarFooter(String saveLocation) {
         super(FXML);
+        setSyncStatus(SYNC_STATUS_INITIAL);
         setSaveLocation("./" + saveLocation);
         registerAsAnEventHandler(this);
     }
 
+    /**
+     * Sets the clock used to determine the current time.
+     */
+    public static void setClock(Clock clock) {
+        StatusBarFooter.clock = clock;
+    }
+
+    /**
+     * Returns the clock currently in use.
+     */
+    public static Clock getClock() {
+        return clock;
+    }
+
+    /**
+     * Sets the displayed location for where the data is stored.
+     */
     private void setSaveLocation(String location) {
         this.saveLocationStatus.setText(location);
+    }
+
+    /**
+     * Sets the displayed sync status message
+     */
+    private void setSyncStatus(String status) {
+        this.syncStatus.setText(status);
+    }
+
+    @Subscribe
+    public void handleEntryBookChangedEvent(EntryBookChangedEvent event) {
+        long now = clock.millis();
+        String lastUpdated = new Date(now).toString();
+        logger.info(LogsCenter.getEventHandlingLogMessage(event, "Setting last updated status to " + lastUpdated));
+        setSyncStatus(String.format(SYNC_STATUS_UPDATED, lastUpdated));
     }
 }
