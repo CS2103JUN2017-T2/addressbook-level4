7bbc0990df61d449cdb3432787ba2a25f93a18a6
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-07 13:02:47 +0800

    Add remaining add command tests

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import guitests.guihandles.EntryCardHandle;
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.ClearCommand;
 import seedu.address.model.entry.Entry;
+import seedu.address.model.entry.Name;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.EntryUtil;
 import seedu.address.testutil.TestUtil;
 
@@ -13,42 +17,118 @@ import seedu.address.testutil.TestUtil;
 public class AddCommandTest extends EntryBookGuiTest {
 
     @Test
-    public void add_floatingTask_success() {
-        Entry[] currentList = typicalEntries.getTypicalFloatingTasks();
+    public void add_floatingTaskToEmptyList_success() {
+        assertCleared();
+        Entry[] currentList = new Entry[0];
         Entry entryToAdd = typicalEntries.spectacles;
         currentList = addFloatingTask(entryToAdd, currentList);
     }
 
     @Test
-    public void add_multipleUniqueFloatingTask_success() {
+    public void add_multipleFloatingTaskToEmptyList_success() {
+        assertCleared();
+        Entry[] currentList = new Entry[0];
+        Entry entryToAdd = typicalEntries.spectacles;
+        currentList = addFloatingTask(entryToAdd, currentList);
+
+        entryToAdd = typicalEntries.clean;
+        currentList = addFloatingTask(entryToAdd, currentList);
+
+        entryToAdd = typicalEntries.sell;
+        currentList = addFloatingTask(entryToAdd, currentList);
+        assertCleared();
+    }
+
+    @Test
+    public void add_floatingTaskToExistingList_success() {
         Entry[] currentList = typicalEntries.getTypicalFloatingTasks();
         Entry entryToAdd = typicalEntries.spectacles;
         currentList = addFloatingTask(entryToAdd, currentList);
-        entryToAdd = typicalEntries.clean;
-        currentList = addFloatingTask(entryToAdd, currentList);
-        entryToAdd = typicalEntries.sell;
-        currentList = addFloatingTask(entryToAdd, currentList);
+        assertCleared();
     }
 
-    /*@Test
-    public void add() {
-        //add duplicate entry
-        commandBox.runCommand(EntryUtil.getAddCommand(td.hoon));
-        assertResultMessage(AddCommand.MESSAGE_DUPLICATE_PERSON);
-        assertTrue(entryListPanel.isListMatching(currentList));
+    @Test
+    public void add_multipleUniqueFloatingTaskToExistingList_success() {
+        Entry[] currentList = typicalEntries.getTypicalFloatingTasks();
+        Entry entryToAdd = typicalEntries.spectacles;
+        currentList = addFloatingTask(entryToAdd, currentList);
 
-        //add to empty list
-        commandBox.runCommand(ClearCommand.COMMAND_WORD);
-        assertAddSuccess(td.alice);
+        entryToAdd = typicalEntries.clean;
+        currentList = addFloatingTask(entryToAdd, currentList);
 
-        //invalid command
-        commandBox.runCommand("adds Johnny");
+        entryToAdd = typicalEntries.sell;
+        currentList = addFloatingTask(entryToAdd, currentList);
+        assertCleared();
+    }
+
+    @Test
+    public void add_unknownCommandName_errorMessage() {
+        commandBox.runCommand("a task");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
-    }*/
 
-    private Entry[] addFloatingTask(Entry entryToAdd, Entry[] currentList) {
+        commandBox.runCommand("ad task");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+
+        commandBox.runCommand("addd task");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    @Test
+    public void add_invalidName_errorMessage() {
+        commandBox.runCommand("add");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("add ");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("add /");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("add $");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("add ?");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("add /ta");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+
+        commandBox.runCommand("add /tag");
+        assertResultMessage(Name.MESSAGE_NAME_CONSTRAINTS);
+    }
+
+    @Test
+    public void add_invalidTags_errorMessage() {
+        commandBox.runCommand("add task /tag");
+        assertResultMessage(Tag.MESSAGE_TAG_CONSTRAINTS);
+
+        commandBox.runCommand("add task /tag /");
+        assertResultMessage(Tag.MESSAGE_TAG_CONSTRAINTS);
+
+        commandBox.runCommand("add task /tag $");
+        assertResultMessage(Tag.MESSAGE_TAG_CONSTRAINTS);
+
+        commandBox.runCommand("add task /tag ?");
+        assertResultMessage(Tag.MESSAGE_TAG_CONSTRAINTS);
+    }
+
+    /**
+     * Adds a floating task to the entry book as well as to the supplied array
+     */
+    private Entry[] addFloatingTask(Entry entryToAdd, Entry... currentList) {
         assertAddFloatingTaskSuccess(entryToAdd, currentList);
         return TestUtil.addEntriesToList(currentList, entryToAdd);
+    }
+
+    /**
+     * Clears all tasks and asserts that they have been cleared
+     */
+    private void assertCleared() {
+        //TODO check archive and bin as well, or check all at once
+        commandBox.runCommand(ClearCommand.COMMAND_WORD);
+        assertEventListEmpty();
+        assertDeadlineListEmpty();
+        assertFloatingTaskListEmpty();
     }
 
     private void assertEventListEmpty() {
