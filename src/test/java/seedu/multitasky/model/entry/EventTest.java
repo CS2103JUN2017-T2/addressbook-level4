79c013bc4189996dcd03f647a743e2fcc902266f
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-07-08 02:40:17 +0800

    Update model component's tests to the state before package name change.

---
+++ src/test/java/seedu/multitasky/model/entry/EventTest.java
@@ -0,0 +1,106 @@
+package seedu.multitasky.model.entry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.multitasky.model.tag.Tag;
+
+public class EventTest {
+
+    Calendar calendar1;
+    Calendar calendar2;
+    Calendar calendar3;
+
+    Set<Tag> tagSet1;
+    Set<Tag> tagSet2;
+
+    Name eventName1;
+    Name eventName2;
+
+    Event tester1;
+    Event tester2;
+    Event tester3;
+    Event tester4;
+    Event tester5;
+    Event tester6;
+
+    @Before
+    public void setUp() {
+        calendar1 = Calendar.getInstance();
+        calendar1.set(2017, 6, 7, 18, 30); // 7th July 2017, 6:30pm
+
+        calendar2 = Calendar.getInstance();
+        calendar2.set(2017, 6, 8, 18, 30); // 8th July 2017, 6:30pm
+
+        calendar3 = Calendar.getInstance();
+        calendar3.set(2017, 6, 9, 18, 30); // 8th July 2017, 6:30pm
+
+        try {
+            tagSet1 = new HashSet<>();
+            tagSet1.add(new Tag("tag1set1"));
+
+            tagSet2 = new HashSet<>();
+            tagSet2.add(new Tag("tag1set2"));
+        } catch (Exception e) {
+            fail("Tags initialisation failed.");
+        }
+
+        try {
+            eventName1 = new Name("SampleName1");
+            eventName2 = new Name("SampleName2");
+        } catch (Exception e) {
+            fail("Event name initialisation failed.");
+        }
+
+        // First tester, used for reference
+        tester1 = new Event(eventName1, calendar1, calendar2, tagSet1);
+        // Same fields as tester1
+        tester2 = new Event(eventName1, calendar1, calendar2, tagSet1);
+        // Only name is different from tester1
+        tester3 = new Event(eventName2, calendar1, calendar2, tagSet1);
+        // Only start time is different from tester1
+        tester4 = new Event(eventName1, calendar2, calendar3, tagSet1);
+        // Only end time is different from tester1
+        tester5 = new Event(eventName1, calendar1, calendar3, tagSet1);
+        // Only tags are different from tester1
+        tester6 = new Event(eventName1, calendar1, calendar2, tagSet2);
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals("error at getName()", "SampleName1", tester1.getName().fullName);
+    }
+
+    @Test
+    public void getTagsTest() {
+        // Same tags
+        assertTrue(tester1.getTags().equals(tester2.getTags()));
+
+        // Different tags
+        assertFalse(tester1.getTags().equals(tester6.getTags()));
+    }
+
+    @Test
+    public void equalsTest() {
+        // Equal
+        assertTrue(tester1.equals(tester2));
+
+        // Not equal
+        assertFalse(eventName1.equals(eventName2));
+        assertFalse(calendar1.equals(calendar2));
+        assertFalse(tester1.equals(tester3));
+        assertFalse(tester1.equals(tester4));
+        assertFalse(tester1.equals(tester5));
+        assertFalse(tester1.equals(tester6));
+    }
+
+}
---
+++ src/test/java/seedu/multitasky/model/entry/FloatingTaskTest.java
@@ -0,0 +1,82 @@
+package seedu.multitasky.model.entry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.multitasky.model.tag.Tag;
+
+public class FloatingTaskTest {
+
+    Set<Tag> tagSet1;
+    Set<Tag> tagSet2;
+
+    Name name1;
+    Name name2;
+
+    FloatingTask tester1;
+    FloatingTask tester2;
+    FloatingTask tester3;
+    FloatingTask tester4;
+
+    @Before
+    public void setUp() throws Exception {
+        try {
+            tagSet1 = new HashSet<>();
+            tagSet1.add(new Tag("tag1set1"));
+
+            tagSet2 = new HashSet<>();
+            tagSet2.add(new Tag("tag1set2"));
+        } catch (Exception e) {
+            fail("Tags initialisation failed.");
+        }
+
+        try {
+            name1 = new Name("SampleName1");
+            name2 = new Name("SampleName2");
+        } catch (Exception e) {
+            fail("Floating task name initialisation failed.");
+        }
+
+        // First tester, used for reference
+        tester1 = new FloatingTask(name1, tagSet1);
+        // Same fields as tester1
+        tester2 = new FloatingTask(name1, tagSet1);
+        // Only name is different from tester1
+        tester3 = new FloatingTask(name2, tagSet1);
+        // Only tags are different from tester1
+        tester4 = new FloatingTask(name1, tagSet2);
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals("error at getName()", "SampleName1", tester1.getName().fullName);
+    }
+
+    @Test
+    public void getTagsTest() {
+        // Same tags
+        assertTrue(tester1.getTags().equals(tester2.getTags()));
+
+        // Different tags
+        assertFalse(tester1.getTags().equals(tester4.getTags()));
+    }
+
+    @Test
+    public void equalsTest() {
+        // Equal
+        assertTrue(tester1.equals(tester2));
+
+        // Not equal
+        assertFalse(tester1.equals(tester3));
+        assertFalse(tester1.equals(tester4));
+    }
+
+}
