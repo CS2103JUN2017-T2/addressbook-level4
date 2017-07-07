eb4a1c9c66d2c3ec3bf6e0a4882bb8364abdf9d1
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-07-08 02:34:21 +0800

    Update FloatingTask.java to state before package name change.

---
+++ src/main/java/seedu/multitasky/model/entry/FloatingTask.java
@@ -0,0 +1,58 @@
+package seedu.multitasky.model.entry;
+
+import java.util.Calendar;
+import java.util.Objects;
+import java.util.Set;
+
+import seedu.multitasky.model.tag.Tag;
+
+public class FloatingTask extends Entry {
+
+    /**
+     * Every field must be present and not null.
+     */
+    public FloatingTask(Name name, Set<Tag> tags) {
+        super(name, tags);
+    }
+
+    @Override
+    public Calendar getStartDateAndTime() {
+        Calendar startDateAndTime = null;
+        return startDateAndTime;
+    }
+
+    @Override
+    public Calendar getEndDateAndTime() {
+        Calendar endDateAndTime = null;
+        return endDateAndTime;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+               || this.isSameStateAs((ReadOnlyEntry) other);
+    }
+
+    /**
+     * Compares the state with another Floating Task.
+     */
+    @Override
+    public boolean isSameStateAs(ReadOnlyEntry other) {
+        return (other instanceof FloatingTask // instanceof handles nulls
+                && this.getName().equals(other.getName()) && this.getTags().equals(other.getTags()));
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(getName(), getStartDateAndTime(), getEndDateAndTime(), getTags());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName()).append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+}
