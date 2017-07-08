713bdc37e0125e84357674855faf779cd9e03a6d
akd13 <akankshitadash@yahoo.com>
2017-07-08 15:38:56 +0800

    Test for XmlAdaptedTag

--- src/main/java/seedu/multitasky/storage/XmlAdaptedTag.java
+++ src/main/java/seedu/multitasky/storage/XmlAdaptedTag.java
@@ -22,21 +22,18 @@ public class XmlAdaptedTag {
 
     /**
      * Converts a given Tag into this class for JAXB use.
-     *
-     * @param source
-     *            future changes to this will not affect the created
+     * future changes to this will not affect the created
      */
     public XmlAdaptedTag(Tag source) {
         tagName = source.tagName;
     }
 
     /**
-     * Converts this jaxb-friendly adapted tag object into the model's Tag
-     * object.
+     * Converts this jaxb-friendly adapted tag object into the
+     * model's Tag object.
      *
-     * @throws IllegalValueException
-     *             if there were any data constraints violated in the adapted
-     *             entry
+     * @throws IllegalValueException if there were any data constraints
+     *             violated in the adapted entry
      */
     public Tag toModelType() throws IllegalValueException {
         return new Tag(tagName);
