55d5b307c079c671fd19e8294c9e58090aceb0cd
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-09 13:37:46 +0800

    Add mixed case delete command word tests

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import seedu.multitasky.commons.core.Messages;
 import seedu.multitasky.commons.core.index.Index;
 import seedu.multitasky.logic.commands.DeleteCommand;
+import seedu.multitasky.logic.parser.CliSyntax;
 import seedu.multitasky.model.entry.Entry;
 import seedu.multitasky.testutil.EntryUtil;
 import seedu.multitasky.testutil.TestUtil;
@@ -58,20 +59,60 @@ public class DeleteCommandTest extends EntryBookGuiTest {
     }
 
     /**
+     * For all mixed-case tests only floating task entries are tested,
+     * which should be suitable to test for all types since the type of task
+     * doesn't affect the parsing of the command word.
+     */
+    @Test
+    public void delete_firstCharUppercase_success() {
+        Entry[] currentList = typicalEntries.getTypicalFloatingTasks();
+        Index targetIndex = TypicalEntries.INDEX_FIRST_ENTRY;
+        char[] commandWord = DeleteCommand.COMMAND_WORD.toCharArray();
+        commandWord[0] = Character.toUpperCase(commandWord[0]);
+        Entry entryToDelete = currentList[targetIndex.getZeroBased()];
+        commandBox.runCommand(String.copyValueOf(commandWord) + " "
+                + CliSyntax.PREFIX_FLOATINGTASK + " " + targetIndex.getOneBased());
+        assertFloatingTaskDeleted(entryToDelete, currentList);
+    }
+
+    @Test
+    public void delete_lastCharUppercase_success() {
+        Entry[] currentList = typicalEntries.getTypicalFloatingTasks();
+        Index targetIndex = TypicalEntries.INDEX_FIRST_ENTRY;
+        char[] commandWord = DeleteCommand.COMMAND_WORD.toCharArray();
+        commandWord[commandWord.length - 1] = Character.toUpperCase(commandWord[commandWord.length - 1]);
+        Entry entryToDelete = currentList[targetIndex.getZeroBased()];
+        commandBox.runCommand(String.copyValueOf(commandWord) + " "
+                + CliSyntax.PREFIX_FLOATINGTASK + " " + targetIndex.getOneBased());
+        assertFloatingTaskDeleted(entryToDelete, currentList);
+    }
+
+    @Test
+    public void delete_middleCharUppercase_success() {
+        Entry[] currentList = typicalEntries.getTypicalFloatingTasks();
+        Index targetIndex = TypicalEntries.INDEX_FIRST_ENTRY;
+        char[] commandWord = DeleteCommand.COMMAND_WORD.toCharArray();
+        commandWord[commandWord.length / 2] = Character.toUpperCase(commandWord[commandWord.length / 2]);
+        Entry entryToDelete = currentList[targetIndex.getZeroBased()];
+        commandBox.runCommand(String.copyValueOf(commandWord) + " "
+                + CliSyntax.PREFIX_FLOATINGTASK + " " + targetIndex.getOneBased());
+        assertFloatingTaskDeleted(entryToDelete, currentList);
+    }
+
+    /**
      * Runs the delete command to delete the floating task at {@code index} and confirms the result is correct.
      * @param currentList A copy of the current list of floating tasks (before deletion).
      */
     private void assertDeleteFloatingTaskSuccess(Index index, final Entry[] currentList) {
         Entry entryToDelete = currentList[index.getZeroBased()];
-        Entry[] expectedRemainder = TestUtil.removeEntryFromList(currentList, index);
-
         commandBox.runCommand(EntryUtil.getFloatingTaskDeleteByIndexCommand(index));
+        assertFloatingTaskDeleted(entryToDelete, currentList);
+    }
 
-        //confirm the list now contains all previous entries except the deleted entry
-        assertTrue(floatingTaskListPanel.isListMatching(expectedRemainder));
-
-        //confirm the result message is correct
-        assertResultMessage(String.format(DeleteCommand.MESSAGE_SUCCESS, entryToDelete));
+    private void assertFloatingTaskDeleted(Entry entryDeleted, final Entry[] currentList) {
+        Entry[] expectedList = TestUtil.removeEntriesFromList(currentList, entryDeleted);
+        assertTrue(floatingTaskListPanel.isListMatching(expectedList));
+        assertResultMessage(String.format(DeleteCommand.MESSAGE_SUCCESS, entryDeleted));
     }
 
 }
