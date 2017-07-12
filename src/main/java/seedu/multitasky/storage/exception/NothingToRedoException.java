3daa872978c2579b85d6595fd0b4665a4f4e74f1
akd13 <akankshitadash@yahoo.com>
2017-07-12 15:33:55 +0800

    Redo command basic implementation

---
+++ src/main/java/seedu/multitasky/storage/exception/NothingToRedoException.java
@@ -0,0 +1,8 @@
+package seedu.multitasky.storage.exception;
+
+public class NothingToRedoException extends Exception {
+
+    public NothingToRedoException(String message) {
+        super(message);
+    }
+}
