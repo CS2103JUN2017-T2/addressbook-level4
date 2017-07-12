152590680f1083e12441d0fa81b887ec87fd0d18
akd13 <akankshitadash@yahoo.com>
2017-07-12 15:37:31 +0800

    Change model and events side for redo

---
+++ src/main/java/seedu/multitasky/commons/events/model/EntryBookToRedoEvent.java
@@ -0,0 +1,38 @@
+package seedu.multitasky.commons.events.model;
+
+import seedu.multitasky.commons.events.BaseEvent;
+import seedu.multitasky.model.EntryBook;
+
+//@@author A0132788U
+/** Indicates the EntryBook in the model has changed */
+public class EntryBookToRedoEvent extends BaseEvent {
+
+    private EntryBook data;
+    private String message;
+
+    public EntryBookToRedoEvent(EntryBook data, String message) {
+        this.data = data;
+        this.message = message;
+    }
+
+    public EntryBook getData() {
+        return data;
+    }
+
+    public void setData(EntryBook data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "undo";
+    }
+}
