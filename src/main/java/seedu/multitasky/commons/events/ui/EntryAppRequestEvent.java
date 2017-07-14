986ad6a314cd274abc831431aaef0a66f189df9c
akd13 <akankshitadash@yahoo.com>
2017-07-14 13:56:40 +0800

    Delete snapshot files during startup instead of exit.

---
+++ src/main/java/seedu/multitasky/commons/events/ui/EntryAppRequestEvent.java
@@ -0,0 +1,30 @@
+package seedu.multitasky.commons.events.ui;
+
+import java.io.File;
+
+// @@author A0132788U
+/**
+ * Deletes all the snapshot files created from previous program run during start.
+ */
+public class EntryAppRequestEvent {
+
+    private static final String FILE_PATH = "data/snapshots/entrybook";
+    private String newFilePath;
+    private boolean ifExists = true;
+    private int index = 1;
+    private File toDelete;
+
+    public void deleteAllSnapshotFiles() {
+
+        while (ifExists == true) {
+            newFilePath = FILE_PATH + index + ".xml";
+            toDelete = new File(newFilePath);
+            ifExists = toDelete.exists();
+            toDelete.delete();
+            index++;
+
+        }
+    }
+    // @@author
+
+}
