908f80b8733e9c72dd9e905abdd8e9d2e462edde
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-20 01:42:47 +0800

    Set help window default size and remove TODO

--- src/main/java/seedu/multitasky/ui/HelpWindow.java
+++ src/main/java/seedu/multitasky/ui/HelpWindow.java
@@ -3,9 +3,11 @@ package seedu.multitasky.ui;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import seedu.multitasky.commons.core.LogsCenter;
 import seedu.multitasky.commons.util.FxViewUtil;
@@ -31,7 +33,10 @@ public class HelpWindow extends UiPart<Region> {
         Scene scene = new Scene(getRoot());
         //Null passed as the parent stage to make it non-modal.
         dialogStage = createDialogStage(TITLE, null, scene);
-        dialogStage.setMaximized(true); //TODO: set a more appropriate initial size
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        //dialogStage.setMaximized(false);
+        dialogStage.setMinWidth(primaryScreenBounds.getWidth() * 0.75);
+        dialogStage.setMinHeight(primaryScreenBounds.getHeight() * 0.75);
         FxViewUtil.setStageIcon(dialogStage, ICON);
 
         String userGuideUrl = getClass().getResource(USERGUIDE_FILE_PATH).toString();
