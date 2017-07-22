42a67f06b5eaf39d0b635a5544f75b2b0332248f
Mattheus Lee <mattheus.lee@gmail.com>
2017-07-22 15:32:04 +0800

    remove unused imports

--- src/main/java/seedu/multitasky/commons/util/match/PermutationMatch.java
+++ src/main/java/seedu/multitasky/commons/util/match/PermutationMatch.java
@@ -1,9 +1,6 @@
 package seedu.multitasky.commons.util.match;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 
 // @@author A0125586X
 /**
